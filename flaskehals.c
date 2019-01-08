ccc07919fb26697ce1ac4d819d067990e9100227
pettert85 <petter@myhostname.localdomain>
2019-01-08 22:57:37 +0100

    D C har fikset ma#

--- flaskehals.c
+++ flaskehals.c
@@ -2,18 +2,39 @@
 #include <unistd.h> 
 #include <stdlib.h>
 #include <stdio.h>
- #include <sys/types.h>
+#include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#define LOKAL_PORT 55556
+#include <string.h>
+#define PORT 8080
 #define BAK_LOGG 10 // Størrelse på for kø ventende forespørsler 
+
+int getFile(){
+
+ //lytte etter getFile
+
+ //søke i rotkatalog etter filnavn og returnere  innhold eller error
+  return -1; // <------------- satt til -1 for å teste logg
+}
+
+int sendResponse(int fd, char * fileContent){
+
+  //send innholdet tilbake til klienten
+  int rv = send(fd,fileContent,strlen(fileContent),0);
+
+    if (rv < 0){
+          perror("send");
+        }
+  
+  return rv;
+}
 
 int main ()
 {
 
-  struct sockaddr_in  lok_adr;
-  int sd, ny_sd,asis_fd,buf;
-  char buffer[BUFSIZ];
+  struct sockaddr_in  lok_adr, client_addr;
+  int sd, ny_sd;
+  socklen_t addr_len = sizeof(struct sockaddr_in);
 
   // Setter opp socket-strukturen
   sd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
@@ -23,12 +44,12 @@ int main ()
 
   // Initierer lokal adresse
   lok_adr.sin_family      = AF_INET;
-  lok_adr.sin_port        = htons((u_short)LOKAL_PORT); 
-  lok_adr.sin_addr.s_addr = htonl(         INADDR_ANY);
+  lok_adr.sin_port        = htons((u_short)PORT); 
+  lok_adr.sin_addr.s_addr = htonl(INADDR_ANY);
 
   // Kobler sammen socket og lokal adresse
   if ( 0==bind(sd, (struct sockaddr *)&lok_adr, sizeof(lok_adr)) )
-    printf("Prosess %d er knyttet til port %d.\n", getpid(), LOKAL_PORT);
+    printf("Webserver har pid: %d og er knyttet til port %d.\n", getpid(), PORT);
   else
     exit(1);
 
@@ -37,41 +58,55 @@ int main ()
   while(1){ 
 
     // Aksepterer mottatt forespørsel
-    ny_sd = accept(sd, NULL, NULL);    
+    ny_sd = accept(sd, (struct sockaddr *)&client_addr, &addr_len);    
 
-    if(0==fork()) {
+    if(0==fork() ) {
 
-      dup2(ny_sd, 1); // redirigerer socket til standard utgang
+      //skriver ut klientens ip-adresse på stdout
+      printf("New connection from IP: %s \n", inet_ntoa(client_addr.sin_addr));
 
-      asis_fd = open("./response.asis",O_RDONLY);
+      //lytte etter forespørsel og se om den eksisterer
+      if (getFile() < 0 ){
+        
+        //fila finnes ikke,send error beskjed til stderr og til bruker
+        sendResponse(ny_sd,"HTTP/1.1 404 NOT FOUND\n");
+        //error_fd = open("./error.log",O_WRONLY);
+        FILE *f = fopen("error.log", "a+");
+
+        //skriver til log fila !!Mangler hendelse (perror)!!
+        fprintf(f,"IP adresse: %s : hendelse:  \n", inet_ntoa(client_addr.sin_addr));
+        fclose(f);
+      }
+
+      else {
+        //filen finnes og sendes  ut til klienten
+        
+        sendResponse(ny_sd,"variabel");
+      }
 
       
-         //dup2(asis_fd, 0); 
+      /*
+      //dup2(ny_sd, 1); // redirigerer socket til standard utgang
 
-      //dup2(asis_fd,0); // redirigerer åpen fil til standard inngang
-
+      asis_fd = open("./response.asis",O_RDONLY);
+      //leser inneholdet i .asis fila og sender til socket.
       while ((buf = read(asis_fd,buffer, BUFSIZ)) > 0 ){
         write(1, buffer, buf);
-      } 
+      }
+        */
 
+      //lukker fila etter bruk
+      //close(asis_fd);
 
-
-
-      close(asis_fd);
-/*
-    
-      printf("HTTP/1.1 200 OK\n");
-      printf("Content-Type: text/plain\n");
-      printf("\n");
-      printf("Hallo klient!\n");
-*/
       // Sørger for å stenge socket for skriving og lesing
       // NB! Frigjør ingen plass i fildeskriptortabellen
-      shutdown(ny_sd, SHUT_RDWR); 
+      shutdown(ny_sd, SHUT_RDWR);
+      printf("%s Connection closed. \n", inet_ntoa(client_addr.sin_addr));
       exit(0);
     }
 
     else {
+      //foreldreprosessen lukker fd og går tilbake å venter på connections.
       close(ny_sd);
     }
   }
