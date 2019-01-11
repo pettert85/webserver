22ee67cc695857dde80b6749b41dc70187c53955
BenniBanjo <bennibanjo80@gmail.com>
2019-01-11 18:06:52 +0100

    Added a solution to handle zombie children and did a little commentary work.

--- flaskehals.c
+++ flaskehals.c
@@ -15,8 +15,9 @@
 #include <fcntl.h>
 #include <string.h>
 #include <time.h>
+#include<sys/wait.h> 
 #define PORT 8080
-#define BAK_LOGG 10 // Størrelse på for kø ventende forespørsler 
+#define BAK_LOGG 10 // Max number of waiting connections
 
 //Global variables
 struct sockaddr_in lok_adr, client_addr;
@@ -27,7 +28,7 @@ int fd;
 
 int errorLog(){
 
-      //log errors to log file
+      //Log errors to log file
       fprintf(stderr, "Client adress: %s: ",inet_ntoa(client_addr.sin_addr));
       perror("ERROR: ");
      
@@ -57,27 +58,27 @@ int receive(int client_sock){
   read_size = recv(client_sock,client_request,6000,0);
   char * file;
  
-  //parse request and extract only the filename
+  //Parse request and extract only the filename
   file = strtok(client_request,"GET /");
   file = strtok(file," ");
 
-  //try opening requested file RONLY
+  //Try opening requested file RONLY
   FILE *filePointer = fopen(file, "r");
 
   //Send file to client if it exists and could be opened
   if( filePointer != NULL) {
     char buf[1000];
     int responseOK = 0;
-    //sends file line by line back to client
+    //Sends file line by line back to client
     while (fgets(buf, sizeof(buf), filePointer) != NULL && responseOK >= 0){
        responseOK = sendResponse(client_sock,buf);
     }      
 
-      fclose(filePointer); //close file again
+      fclose(filePointer); //Close file again
       return 0;
   }
 
-  //else the file does not exist send 404 error
+  //Else the file does not exist send 404 error
   else{
     sendResponse(client_sock,"HTTP/1.1 404 NOT FOUND\n");
     fclose(filePointer);
@@ -89,57 +90,61 @@ int receive(int client_sock){
 
 int main () {
     
-  //STDERR points to  log file
+  //STDERR points to log file
   char per[] = "error.log";
   fd = open(per,O_APPEND | O_CREAT | O_WRONLY,00660);
   dup2(fd,2);
       
-  // Setter opp socket-strukturen
+  //Setter opp socket-strukturen
   sd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
-  // For at operativsystemet ikke skal holdte porten reservert etter tjenerens død
+  //For at operativsystemet ikke skal holdte porten reservert etter tjenerens død
   setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, &(int){ 1 }, sizeof(int));
 
-  // Initierer lokal adresse
+  //Initiate local address
   lok_adr.sin_family      = AF_INET;
   lok_adr.sin_port        = htons((u_short)PORT); 
   lok_adr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-  // Kobler sammen socket og lokal adresse
+  //Binds socket and local address
   if ( 0==bind(sd, (struct sockaddr *)&lok_adr, sizeof(lok_adr)) )
     fprintf(stderr,"Webserver has pid: %d and is using port %d.\n\n", getpid(), PORT);
   else
     exit(1);
 
-  // Venter på forespørsel om forbindelse
+  //Waiting for connection
   listen(sd, BAK_LOGG); 
   while(1){ 
 
-    // Aksepterer mottatt forespørsel
+    //Accepts incoming connection
     client_sock = accept(sd, (struct sockaddr *)&client_addr, &addr_len);    
 
     if(0==fork() ) {
 
-      //change STDIN and STDOUT to client
+      //Change STDIN and STDOUT to client
 
 
-      //log client requests
+      //Log client requests
       fprintf(stderr,"New request from %s. \n", inet_ntoa(client_addr.sin_addr));
 
-      //receive request from client.
+      //Receive request from client.
       receive(client_sock);
 
-      // close socket and free fd space
+      //Close socket and free fd space
       shutdown(client_sock, SHUT_RDWR);
 
-      //log closed connections
+      //Log closed connections
       fprintf(stderr,"Connection to %s closed. \n\n", inet_ntoa(client_addr.sin_addr));
       exit(0);
       
     }
 
     else {
-      //foreldreprosessen lukker fd og går tilbake å venter på connections.
+      
+      //The system ignores the signal given by the child upon termination and no zombie is created.
+      signal(SIGCHLD,SIG_IGN); 
+
+      //The parent process closes the filedescriptor and returns to wait for incoming connections.
       close(client_sock);
     }
   }
