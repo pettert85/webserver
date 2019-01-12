005137fd0a7e216584bfd691701a6157c4a37f82
pettert85 <petter@myhostname.localdomain>
2019-01-12 16:24:09 +0100

    Code cleanup

--- flaskehals.c
+++ flaskehals.c
@@ -21,14 +21,13 @@
 
 //Global variables
 struct sockaddr_in lok_adr, client_addr;
-int sd, client_sock;
+int sd, client_sock, fd;
 socklen_t addr_len = sizeof(struct sockaddr_in);
 
-int fd;
 
 int errorLog(){
 
-      //Log errors to log file
+      //Log information and perrror to "error.log" file
       fprintf(stderr, "Client adress: %s: ",inet_ntoa(client_addr.sin_addr));
       perror("ERROR: ");
      
@@ -37,12 +36,11 @@ int errorLog(){
 
 int sendResponse(int client_sock, char *fileContent){
 
-//!!!!!!!!!!!!!!!!!!!HUSK OG ENDRE STATEMENT!!!!!!!!!!
-  //send innholdet tilbake til klienten
+  //Sends data from *fileContent back through client_sock
   if (send(client_sock,fileContent,strlen(fileContent),0) == -1 ){
         errorLog();
         
-        return -1;
+        return -1; 
   }
   
   else {return 0;}
@@ -57,26 +55,29 @@ int receive(int client_sock){
   //Read clients request (GET)
   read_size = recv(client_sock,client_request,6000,0);
   char *file;
-  printf("%s\n",client_request ); 
 
   /*
   strtok splits the string "GET /html/info.asis HTTP/1.1" into separate words:
   Get, /html/info.asis and HTTP/1.1 using escape character. 
   The line " file = strtok (NULL, " "); " moves to the 2nd word. /html/info.asis
-  then we simply chop off the first char:
+  then we simply chop off the first char so it looks like:
   html/info.asis
   */
-  
+
   file = strtok (client_request," ");
   file = strtok (NULL, " "); //show 2nd word
-  char* chopped_file = file + 1; //remove first char /
+  file = file + 1; //remove first char /
 
-  FILE *filePointer = fopen(chopped_file, "r");
+  FILE *filePointer = fopen(file, "r");
 
   //Send file to client if it exists and could be opened
   if( filePointer != NULL) {
     char buf[1000];
     int responseOK = 0;
+   
+    //char * tei = "HTTP/1.1 200 OK\r\n Content-Type: image/jpg\r\n Content-Length: 353486\r\n\r\n";
+    //send(client_sock,tei,strlen(tei),0);
+
     //Sends file line by line back to client
     while (fgets(buf, sizeof(buf), filePointer) != NULL && responseOK >= 0){
        responseOK = sendResponse(client_sock,buf);
@@ -86,8 +87,8 @@ int receive(int client_sock){
       return 0;
   }
 
-  //Else the file does not exist send 404 error
-  else{
+  
+  else{ //The file does not exist send 404 back to client
     sendResponse(client_sock,"HTTP/1.1 404 NOT FOUND\n");
     fclose(filePointer);
     return -1;
@@ -117,20 +118,21 @@ int main () {
   //Binds socket and local address
   if ( 0==bind(sd, (struct sockaddr *)&lok_adr, sizeof(lok_adr)) )
     fprintf(stderr,"Webserver has pid: %d and is using port %d.\n\n", getpid(), PORT);
-  else
-    exit(1);
 
-  //Waiting for connection
-  listen(sd, BAK_LOGG); 
+  else { //something went wrong
+    errorLog();
+    exit(1); 
+  }
+
+  //Waiting for incoming connection
+  listen(sd, BAK_LOGG);
+
   while(1){ 
 
     //Accepts incoming connection
     client_sock = accept(sd, (struct sockaddr *)&client_addr, &addr_len);    
 
     if(0==fork() ) {
-
-      //Change STDIN and STDOUT to client
-
 
       //Log client requests
       fprintf(stderr,"New request from %s. \n", inet_ntoa(client_addr.sin_addr));
